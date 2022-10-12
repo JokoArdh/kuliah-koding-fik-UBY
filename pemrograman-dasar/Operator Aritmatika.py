a670abd6a8ce7d8a4928caa6c84e6cc163af020f
Akanechan30 <efiwaasi30@gmail.com>
2022-10-12 19:19:09 +0700

    Praktikum 1 Aritmatika & Perbandingan

---
+++ pemrograman-dasar/Operator Aritmatika.py
@@ -0,0 +1,15 @@
+print ("===================Luas Persegi Panjang======================")
+p = float (input("Masukan Nilai Panjang :"))
+l = float (input("Masukan Nilai Lebar :"))
+
+Luas = p*l
+
+print("Luas Persegi Panjang Adalah :",Luas,"cm")
+
+print ("===================Keliling Persegi Panjang======================")
+p = float (input("Masukan Nilai Panjang :"))
+l = float (input("Masukan Nilai Lebar :"))
+
+Keliling = 2 * (p + l)
+
+print("Keliling Persegi Panjang Adalah :",Keliling,"cm")
---
+++ pemrograman-dasar/Perbandingan.py
@@ -0,0 +1,12 @@
+print("=============Program Nilai==============\n")
+
+nilai1 = float (input("Masukan Nilai 1 :\n"))
+nilai2 = float (input("Masukan Nilai 2 :\n"))
+nilai3 = float (input("Masukan Nilai 3 :\n"))
+nilai4 = float (input("Masukan Nilai 4 :\n"))
+
+Ratarata = (nilai1 + nilai2 + nilai3 + nilai4)/ 4
+
+print("rata rata anda Adalah :",Ratarata)
+if Ratarata > 70  : print("Anda Lulus!!!")
+if Ratarata < 70 : print("Anda Tidak Lulus!!!")
