79393b4628a4d21484df110cfb01fc608f818fee
JokoArdh <c20010007@uby.ac.id>
2022-10-21 21:37:51 +0700

    upload fundamenttal variabel dan tipe data

---
+++ Javascript/Fundamental/js2.js
@@ -0,0 +1,23 @@
+// membuat variabel pada javascript
+
+// cara 1
+let halo;
+halo = "haiiii saya anak IT";
+
+//cara ke 2
+let nama = "jiyan ambrusilam";
+let x = 120;
+let y = 12.4;
+
+// apa itu const ??
+// const atau konstanta adalah variabel yang nilainya tidak dapat dirubah atau permanen beda ngan let
+// jika let ia bersifat dinamis dan berubah ubah
+
+const i = 50; // nilai ini tetap dan bersifat permanen
+console.log(i)
+
+
+console.log(halo);
+console.log(nama);
+console.log(x);
+console.log(y);
---
+++ Javascript/Fundamental/js3.js
@@ -0,0 +1,29 @@
+//latihan tipe data
+
+// 1. tipe data angka / number
+let a = 6;
+let b = 3;
+
+console.log(a + b);
+console.log(typeof(a)); // untuk ngecek apakah benar tipe data number
+
+// 2. tipe string
+let she = "diana";
+
+console.log(typeof(she));
+
+// mengenal sistem literal atau tempalting string pada javascript
+let ucapan = " selamat malam";
+
+console.log(`Halo, Pak Joko ${ucapan}`); // ini disebut literal atau menampilkan variabel alngsung berdanpingan pda strings
+
+
+// 3. boolean
+const satu = 50;
+const dua = 10;
+
+let isbesar = satu > dua;
+let iskecil = satu < dua;
+
+console.log(isbesar);
+console.log(iskecil);
